d41e7b0d37f5f20baff47fc8d887a927ae985600
Dupont ARTHUR <arthur.dupont@isl-edu.be>
2026-01-12 14:00:08 +0000

    first commit

---
+++ _config/config.php
@@ -0,0 +1,8 @@
+<?php
+
+date_default_timezone_set("europe/brussels");
+define("SERVICE_PATH", './');
+
+error_reporting(E_ALL | E_STRICT);
+ini_set('error_log', __DIR__ . '/../logs/' . 'error_log.log');
+ini_set('log_errors', 'On');
---
+++ public/launch.php
@@ -0,0 +1,6 @@
+<?php
+
+require(__DIR__ . '/../_config/config.php');
+require(__DIR__ . '/../vendor/autoload.php');
+
+echo "coucou";
