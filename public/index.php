66d63ee3fd522d409156f072e2e1f5720456b2c9
Dupont ARTHUR <arthur.dupont@isl-edu.be>
2026-02-04 11:04:26 +0000

    refactor: Update home and test views, enhance filter functionality, and improve script organization

--- public/index.php
+++ public/index.php
@@ -17,19 +17,18 @@ $errorMiddleware = $app->addErrorMiddleware(true, true, true);
 
 // Teste la connexion au LRS
 //
-$lrsChecker = new LRSConnectionCheck();
+// $lrsChecker = new LRSConnectionCheck();
+// echo "Pinging LRS... ";
+// echo $lrsChecker->pingLRS() ? 'LRS is reachable.' : 'Failed to reach LRS.';
 
-echo "Pinging LRS... ";
-echo $lrsChecker->pingLRS() ? 'LRS is reachable.' : 'Failed to reach LRS.';
-//
 // error_log($_SERVER["REQUEST_URI"]);
 
 
-// $app->get('/', function (Request $request, Response $response,) {
-//     return $response->withHeader('Location', 'Views/home.html')->withStatus(302);
-// });
+$app->get('/', function (Request $request, Response $response,) {
+    return $response->withHeader('Location', 'Views/home.html')->withStatus(302);
+});
 
-// $routes = require_once(__DIR__ . '/../src/Routes/route_test.php');
+$routes = require_once(__DIR__ . '/../src/Routes/route_test.php');
 
-// $routes($app);
-// $app->run();
+$routes($app);
+$app->run();
